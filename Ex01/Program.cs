c101fd219bf98ad1ba5088982bdbc79e6e8c4450
NEVA <neva.vka@yandex.ru>
2022-12-06 12:40:44 +0300

    Написали программу

--- Ex01/Program.cs
+++ Ex01/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+
+string [] array = {"Moskow", "Tom", "Andrey", "25"};
+
+Console.Write("[ ");
+for(int i = 0; i < array.Length; i++ )
+{
+    Console. Write($"{array[i]}, ");
+}
+Console.Write("]");
+Console.WriteLine();
+
+string [] array1 = new string[10];
+Console.Write("[ ");
+for(int i = 0; i < array.Length; i++ )
+{
+    if (array[i].Length <=3)
+    {
+        array1[i] = array[i];
+       Console. Write($"{array1[i]}, ");
+    }
+}
+Console.Write("]");
